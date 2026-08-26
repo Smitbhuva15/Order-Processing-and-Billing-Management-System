c6459e43563abcbf810c46542e5efa5ff0a1c0e9
Smitbhuva15 <itssmit123@gmail.com>
2026-08-26 14:06:22 +0530

    feat: add order and billing repositories

--- src/main/java/com/egen/OrderSystemApplication.java
+++ src/main/java/com/egen/OrderSystemApplication.java
@@ -21,4 +21,26 @@ public class OrderSystemApplication {
 		SpringApplication.run(OrderSystemApplication.class, args);
 	}
 	
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build()
+          .apiInfo(apiDetails());
+        
+	}
+	
+	private ApiInfo apiDetails() {
+		return new ApiInfo(
+				"Order Management API",
+				"This API is for Order Management Microservice",
+				"1.0",
+				"Free to use",
+				new springfox.documentation.service.Contact("Tushar", "https://tushar2805.github.io/portfolio/", "yusharutd@gmail.com"),
+				"API RELEASE",
+				"http://",
+				Collections.emptyList());
+	}
 }
